a9b62af2a40e5bbcfcd2b1716f4ca20e6e1d2a4f
AzkaKhizer <144557215+AzkaKhizer@users.noreply.github.com>
2024-11-27 21:20:03 +0500

    layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";    // import {Inter} from "next/font/google"
 import "./globals.css";
+import Header from "@/components/Header";
+import 
 
 
 
